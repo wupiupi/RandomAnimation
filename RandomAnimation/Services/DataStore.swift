ed8ff5b4304857cf5dbee5cb113a3591c9f80900
Paul <pasha.makey@gmail.com>
2024-01-25 21:39:46 +0300

    🔧 chore: Service class for storing the data

---
+++ RandomAnimation/Services/DataStore.swift
@@ -0,0 +1,74 @@
+//
+//  DataStore.swift
+//  RandomAnimation
+//
+//  Created by Paul Makey on 25.01.24.
+//
+
+final class DataStore {
+    static let shared = DataStore()
+    
+    let presets = [
+        "pop",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeOutIn",
+        "fadeInLeft",
+        "fadeInRight",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+        "fall",
+        "shake",
+        "flipX",
+        "flipY",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing"
+    ]
+    
+    let curves = [
+        "easeIn",
+        "easeOut",
+        "easeInOut",
+        "linear",
+        "spring",
+        "easeInSine",
+        "easeOutSine",
+        "easeInOutSine",
+        "easeInQuad",
+        "easeOutQuad",
+        "easeInOutQuad",
+        "easeInCubic",
+        "easeOutCubic",
+        "easeInOutCubic",
+        "easeInQuart",
+        "easeOutQuart",
+        "easeInOutQuart",
+        "easeInQuint",
+        "easeOutQuint",
+        "easeInOutQuint",
+        "easeInExpo",
+        "easeOutExpo",
+        "easeInOutExpo",
+        "easeInCirc",
+        "easeOutCirc",
+        "easeInOutCirc",
+        "easeInBack",
+        "easeOutBack",
+        "easeInOutBack"
+    ]
+    
+    private init() {}
+}
